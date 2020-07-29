9944ccc5add1025d57818fe64cfd7f303fe11524
buteomont <buteomont@gmail.com>
2020-07-28 21:25:52 -0500

    commit test

--- startRTL.sh
+++ startRTL.sh
@@ -4,6 +4,7 @@ echo `date` >>/var/log/rtl/rtl1.log
 sleep 10s
 nohup /usr/local/bin/rtl_433 -f 433.92M -M level -F mqtt:localhost:1883 >>/var/log/rtl/rtl1.log 2>&1 &
 disown
+
 sleep 5s
 echo -e '\n--------------------\n' >>/var/log/rtl/rtl2.log
 echo `date` >>/var/log/rtl/rtl2.log
