9d2cc6e4c52c766fe7a23ec757db081ad4913ef7
buteomont <buteomont@gmail.com>
2020-07-28 21:07:38 -0500

    added hard link for rtl_433 configuration file

--- startRTL.sh
+++ startRTL.sh
@@ -2,11 +2,11 @@
 echo -e '\n--------------------\n' >>/var/log/rtl/rtl1.log
 echo `date` >>/var/log/rtl/rtl1.log
 sleep 10s
-nohup /usr/local/bin/rtl_433 -f 433.92M -M newmodel -F mqtt:localhost:1883 >>/var/log/rtl/rtl1.log 2>&1 &
+nohup /usr/local/bin/rtl_433 -f 433.92M -M level -F mqtt:localhost:1883 >>/var/log/rtl/rtl1.log 2>&1 &
 disown
 sleep 5s
 echo -e '\n--------------------\n' >>/var/log/rtl/rtl2.log
 echo `date` >>/var/log/rtl/rtl2.log
-#nohup /usr/local/bin/rtl_433 -f 310.422M -M newmodel -R 22 -F mqtt:localhost:1883 >>/var/log/rtl/rtl2.log 2>&1 &
+#nohup /usr/local/bin/rtl_433 -f 310.422M -M level -R 22 -F mqtt:localhost:1883 >>/var/log/rtl/rtl2.log 2>&1 &
 disown
 
